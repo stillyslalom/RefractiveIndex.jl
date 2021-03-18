a97f5a5c79268d7c08dc237f632a93806ea5ed16
Documenter.jl <documenter@juliadocs.github.io>
2021-03-18 02:17:33 +0000

    build based on b2b7c50

---
+++ dev/search_index.js
@@ -0,0 +1,3 @@
+var documenterSearchIndex = {"docs":
+[{"location":"","page":"Home","title":"Home","text":"CurrentModule = RefractiveIndex","category":"page"},{"location":"#RefractiveIndex","page":"Home","title":"RefractiveIndex","text":"","category":"section"},{"location":"","page":"Home","title":"Home","text":"","category":"page"},{"location":"","page":"Home","title":"Home","text":"Modules = [RefractiveIndex]","category":"page"}]
+}
