191ddec8af495ba0457efa5e49e417192b6a3380
Alex Ames <alexander.m.ames@gmail.com>
2024-08-16 21:44:51 -0600

    refractiveindex.info v2024-08-14

--- deps/add_artifact.jl
+++ deps/add_artifact.jl
@@ -2,7 +2,7 @@ using ArtifactUtils, Artifacts
 add_artifact!(
     "../Artifacts.toml",
     "refractiveindex.info",
-    "https://github.com/polyanskiy/refractiveindex.info-database/archive/v2023-10-04.tar.gz",
+    "https://github.com/polyanskiy/refractiveindex.info-database/archive/v2024-08-14.tar.gz",
     # lazy=true, # need to use LazyArtifacts for this
     force=true,
     clear=false,
